a16f9d5dc2ef6d501a108faec70d8a8c56f7cb3d
Moses Buckwalter <mosesbuckwalter@users.com>
2021-04-20 08:29:21 -0400

    Math

--- GPA Calculator/main.swift
+++ GPA Calculator/main.swift
@@ -8,69 +8,25 @@
 import Foundation
 
 print("Hello, World!")
-
 func GPArray() -> Array <String> {
     let path = "/Users/mosesbuckwalter/Developer/Xcode/GPA Calculator/GPA Calculator/gpa.txt"
     do {
-            let numbers = try String(contentsOfFile: path).components(separatedBy: "/n")
-            return numbers
-    }
+        let combined = try String(contentsOfFile: path).components(separatedBy: "")
+        return combined
+        } catch {
+            return [String]()
+        }
 }
 
+let GPAString = GPArray().joined(separator: "")
+let pairs = GPAString.components(separatedBy: "\n")
+print(pairs)
 
-let (Classtype, lettergrade)
+let GPAValues = [1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 9.5, 13.5, 12.5, 15.5, 18.5, 35.5, 76.5, 9.5]
 
-struct Color {
-    let red, green, blue: Double
-    init(red: Double, green: Double, blue: Double) {
-        self.red   = red
-        self.green = green
-        self.blue  = blue
-    }
-    init(white: Double) {
-        red   = white
-        green = white
-        blue  = white
-    }
-}
-let magenta = Color(red: 1.0, green: 0.0, blue: 1.0)
-let halfGray = Color(white: 0.5)
-struct GPA1 {
-    let "AP+", "AP", "Honors", "CP", "Career": Double
-    init("AP+": Double, "AP": Double, "Honors": Double, "CP": Double, "Career": Double) {
-        self."AP+"   = red
-        self."AP" = green
-        self."Honors"  = blue
-        self."CP"  = blue
-        self."Career"  = blue
-    }
-}
-let mygpa = Color(red: 1.0, green: 0.0, blue: 1.0)
-
-
-struct LetterGrade {
-    var a, b, c, d, f
-}
-struct ClassType {
-    var x = 0.0, y = 0.0
-}
-struct GPA {
-    var Class = ClassType()
-    var Grade = LetterGrade()
-    init() {}
-    init(Class: ClassType, Grade: LetterGrade) {
-        self.ClassType = ClassType
-        self.Grade = Grade
-    }
-}
-
-
-
-
-let ClassGrade = "[[39.86475483576405,-75.53281903266907], [39.864688955564304,-75.53292632102966], [39.86455719497505,-75.53300142288208], [39.86440072894666,-75.5330228805542], [39.8642689678039,-75.53295850753784], [39.863305456757146,-75.53223967552185], [39.86303369478483,-75.53266882896423]]"
-
-if let data = ClassGrade.data(using: .utf8),
-    let jsonArray = try? JSONSerialization.jsonObject(with: data) as? [[Double]] {
-    let ClassGradeArray  = jsonArray!.map{CLLocation(Class:$0[0], Grade:$0[1]) }
-    print(ClassGradeArray)
-}
+let Sum = GPAValues.reduce(0, +)
+let GPASum = Sum
+print(GPASum)
+let GPANumber = GPAValues.count
+var GPA:Double=(GPASum/Double(GPANumber))
+print(GPA)
